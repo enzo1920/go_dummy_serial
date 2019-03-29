f48e461fbc932e230e2e83ef0cc49af1d3890a9f
enzo1920 <enzo1920@ya.ru>
2019-03-29 15:02:26 +0300

    work with two buffers for read

--- dummy_serial.go
+++ dummy_serial.go
@@ -15,7 +15,8 @@ const AppVersion = "1.0.4 beta"
 
 func check(e error) {
 	if e != nil {
-		panic(e)
+		log.Fatal(e)
+		log.Print("\r\n")
 	}
 }
 
@@ -58,13 +59,25 @@ func main() {
 	comport := "COM" + strconv.Itoa(port)
 
 	byte_arr := []byte("= ")
-	/*
-		testrr := []byte("=021000 ")
-		fmt.Printf("\n testarr_str %v", testrr)
-		testrrr1 := reverse(testrr)
-		fmt.Printf("\n testrrr1 %s", testrrr1)
-		_, err1 := f.Write(testrrr1)
-		check(err1)*/
+	
+	//"=021000 "
+	//testrr := []byte("xyz")
+	/*fmt.Printf("\n testarr_str %v", testrr)
+	testrrr1 := []byte("87659")
+	fmt.Printf("\n testrrr1 %s", testrrr1)
+	_, err1 := f.Write(testrrr1)
+	check(err1)
+	thirdarr:= append(testrr ,testrrr1...)
+	fmt.Printf("\n thirdarr %s", thirdarr)*/
+/*
+	b := make([]byte, 8)
+	b = []byte("87665966")
+	b = append(b, testrr...)
+	b = append(b, byte_arr...)
+	fmt.Printf("\n b %s", b)
+	b = b[:0]
+	fmt.Printf("\n len(b) %s cap(b)%s", len(b), cap(b))
+	*/
 
 	log.Printf("port:%s, speed %d, timeout: %d \r\n", comport, speed, timeout)
 	c := &serial.Config{Name: comport, Baud: speed, ReadTimeout: time.Duration(timeout) * time.Second}
@@ -75,36 +88,60 @@ func main() {
 		return
 	}
 	buf := make([]byte, 8)
+	tmp_buf := make([]byte, 8)
+	
+	//buf1 :=buf[:n]
+		for {
+			n, errr := stream.Read(buf)
+	        check(errr)
+	        fmt.Println("i read: ", n)
+	        fmt.Printf("\nbuffer:%v", buf)
+			k, errr := stream.Read(tmp_buf)
+			check(errr)
+			fmt.Printf("\ni read k-bytes: %v", k)
+			fmt.Printf("\ntmp_buf:%v", tmp_buf)
+			time.Sleep(time.Duration(timeout) * time.Second)
+	        if n > 0 || k > 0{
+				for i := 0;  i<=k; i++ {
+					if n+i == len(buf) { 
+						break 
+					}else{
+						buf[n+i] = tmp_buf[i]
+					}
+	
+				}
+				fmt.Printf("\n add to buf? now  buffer contains :%v", buf)
+				//clear tmp_buf
+				tmp_buf = tmp_buf[:0]
+				n = n+k
+				if n >= len(buf) {
+					if buf[0] == byte_arr[0] && buf[7] == byte_arr[1] {
+						fmt.Println("Found! exit!")
+						log.Printf("\n 0x3d and 0x20 found in place [0] and [7] exit!")
+						fmt.Printf("Found! %s\n", buf)
+						fmt.Printf("Reversed buff and write in results! %s\n", reverse(buf))
+						n2, err := f.Write(reverse(buf))
+						check(err)
+						fmt.Printf("wrote %d bytes\n", n2)
+			
+						log.Printf("\n Stop logging!")
+						log.Print("\r\n")
+			
+						os.Exit(0)
+					} else {
+							fmt.Printf("\nNot found in: %v", buf)
+							buf = buf[:0]
+							tmp_buf = tmp_buf[:0]
+						}
+					 //break 
+					}
+			} 
+			
+        }
 
-	for {
-		n, errr := stream.Read(buf)
-		if errr != nil {
-			log.Fatal(errr)
-			log.Print("\r\n")
-		}
-		fmt.Println("i read: ", n)
-		fmt.Printf("\nbuffer:%v", buf)
 
-		if n == 8 {
-			if buf[0] == byte_arr[0] && buf[7] == byte_arr[1] {
-				fmt.Println("Found! exit!")
-				log.Printf("\n 0x3d and 0x20 found in place [0] and [6] exit!")
-				fmt.Printf("Found! %s\n", buf)
-				fmt.Printf("Reversed buff and write in results! %s\n", reverse(buf))
-				n2, err := f.Write(reverse(buf))
-				check(err)
-				fmt.Printf("wrote %d bytes\n", n2)
+	
 
-				log.Printf("\n Stop logging!")
-				log.Print("\r\n")
 
-				os.Exit(0)
-			} else {
-				fmt.Printf("\nNot found in: %s", buf)
-			}
-
-		}
-
-	}
 
 }
