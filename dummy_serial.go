d660927d114b0bff616eda69542574d9cbd11c08
enzo1920 <enzo1920@ya.ru>
2019-04-03 17:19:35 +0300

    remove results.txt on start

--- dummy_serial.go
+++ dummy_serial.go
@@ -11,7 +11,7 @@ import (
 	"github.com/tarm/serial"
 )
 
-const AppVersion = "1.0.5 beta"
+const AppVersion = "1.0.6 beta"
 
 func check(e error) {
 	if e != nil {
@@ -28,38 +28,56 @@ func reverse(array_byte []byte) []byte {
 	return array_byte
 }
 
-func Comparer (need_to_comp []byte, f *os.File) bool{
+func Comparer(need_to_comp []byte, fname string) bool {
 	byte_arr := []byte("= ")
-    var comp bool
+	var comp bool
 	if need_to_comp[0] == byte_arr[0] && need_to_comp[7] == byte_arr[1] {
 		fmt.Println("Found! exit!")
 		log.Printf("\n 0x3d and 0x20 found in place [0] and [7] exit!")
 		fmt.Printf("Found! %s\n", need_to_comp)
 		fmt.Printf("Reversed buff and write in results! %s\n", reverse(need_to_comp[1:7]))
+
+		log.Printf("\n Stop logging!")
+		log.Print("\r\n")
+		comp = true
+
+		//create result file
+		f, err := os.OpenFile(fname, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+		check(err)
+		defer f.Close()
 		//chislo:=reverse(need_to_comp[1:7])
 		n2, err := f.Write(need_to_comp[1:7])
 		check(err)
 		fmt.Printf("wrote %d bytes\n", n2)
 
-		log.Printf("\n Stop logging!")
-		log.Print("\r\n")
-		comp =true
-
 		//os.Exit(0)
 	} else {
-			fmt.Printf("\nNot found in: %v", need_to_comp)
-			comp=false
-			//buf = buf[:0]
-			//tmp_buf = tmp_buf[:0]
-			//n=0
-		}
-		return comp
+		fmt.Printf("\nNot found in: %v", need_to_comp)
+		comp = false
+		//buf = buf[:0]
+		//tmp_buf = tmp_buf[:0]
+		//n=0
+	}
+	return comp
 }
 
+func Exists(name string) bool {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
 
 func main() {
 
 	fmt.Println(AppVersion)
+
+	fileresults := "./results.txt"
+
+	if Exists(fileresults) {
+		os.Remove(fileresults)
+	}
+
 	file, err := os.OpenFile("dummy_serial.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
@@ -69,15 +87,6 @@ func main() {
 	log.SetOutput(file)
 	log.Printf("\n Start logging!")
 	log.Print("\r\n")
-
-	//create result file
-	f, err := os.OpenFile(
-        "results.txt",
-        os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
-        0666,
-    )
-	check(err)
-	defer f.Close()
 
 	//parse flags
 	var port int
@@ -91,9 +100,7 @@ func main() {
 
 	comport := "COM" + strconv.Itoa(port)
 
-	
 	//"=021000 "
-
 
 	log.Printf("port:%s, speed %d, timeout: %d \r\n", comport, speed, timeout)
 	c := &serial.Config{Name: comport, Baud: speed, ReadTimeout: time.Duration(timeout) * time.Second}
@@ -105,50 +112,49 @@ func main() {
 	}
 	buf := make([]byte, 8)
 	tmp_buf := make([]byte, 8)
-	
-	//buf1 :=buf[:n]
-		for {
-			n, errr := stream.Read(buf)
-	        check(errr)
-	        //fmt.Printf("\ni read: %v", n)
-			//fmt.Printf("\nbuffer:%v", buf)
-			if n < len(buf){
-                for{
-					
-					k, errr := stream.Read(tmp_buf)
-					check(errr)
-					if k > 0{
-						//fmt.Printf("\ni read k-bytes: %v", k)
-					    //fmt.Printf("\ntmp_buf:%v", tmp_buf)
-					    //fmt.Printf("\n k n: %v %v", k,n)
-					    for i := 0;  i<k; i++ {
-							//fmt.Printf("\n len buf: %v %v ", len(buf), n)
-						    if n >= len(buf) {
-								//fmt.Printf("\n stop need compare!")
-							            break 
-						    }else{
-							    //fmt.Printf("\n tmp_buf[i]: %v ", tmp_buf[i])
-								buf[n+i] = tmp_buf[i]
-								
-						    }
-							//fmt.Printf("\n add to buf! now  buffer contains :%v %s", buf, buf)
-							
-					}
-					n = n+k
-					}else{
-						break 
-					}
-					//clear tmp_buf
-					fmt.Printf("\nadd to buf! now  buffer contains :%v %s", buf, buf)
-					tmp_buf = tmp_buf[:0]
-				    
-			        
-				}
-			}
-			//fmt.Printf("\n run comparer")
-			if Comparer(buf,f) {
-				os.Exit(0)
-			}
 
+	//buf1 :=buf[:n]
+	for {
+		n, errr := stream.Read(buf)
+		check(errr)
+		//fmt.Printf("\ni read: %v", n)
+		//fmt.Printf("\nbuffer:%v", buf)
+		if n < len(buf) {
+			for {
+
+				k, errr := stream.Read(tmp_buf)
+				check(errr)
+				if k > 0 {
+					//fmt.Printf("\ni read k-bytes: %v", k)
+					//fmt.Printf("\ntmp_buf:%v", tmp_buf)
+					//fmt.Printf("\n k n: %v %v", k,n)
+					for i := 0; i < k; i++ {
+						//fmt.Printf("\n len buf: %v %v ", len(buf), n)
+						if n >= len(buf) {
+							//fmt.Printf("\n stop need compare!")
+							break
+						} else {
+							//fmt.Printf("\n tmp_buf[i]: %v ", tmp_buf[i])
+							buf[n+i] = tmp_buf[i]
+
+						}
+						//fmt.Printf("\n add to buf! now  buffer contains :%v %s", buf, buf)
+
+					}
+					n = n + k
+				} else {
+					break
+				}
+				//clear tmp_buf
+				fmt.Printf("\nadd to buf! now  buffer contains :%v %s", buf, buf)
+				tmp_buf = tmp_buf[:0]
+
+			}
 		}
+		//fmt.Printf("\n run comparer")
+		if Comparer(buf, fileresults) {
+			os.Exit(0)
+		}
+
 	}
+}
